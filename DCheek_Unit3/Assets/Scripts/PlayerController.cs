a41d2561dda75fd807908676d30098b68c07ac4c
IDJPunkI <138734945+IDJPunkI@users.noreply.github.com>
2024-10-11 18:15:21 -0700

    Lesson C completed

--- DCheek_Unit3/Assets/Scripts/PlayerController.cs
+++ DCheek_Unit3/Assets/Scripts/PlayerController.cs
@@ -10,23 +10,29 @@ public class PlayerController : MonoBehaviour
     private bool onGround = true;
     public bool gameOver = false;
 
+    private Animator animPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         Physics.gravity = Physics.gravity * gravityModifier;
+
+        animPlayer = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         bool spaceDown = Input.GetKeyDown(KeyCode.Space);
+        // Conditions met to jump!
         if (spaceDown && onGround && !gameOver)
         {
             // jump code
             rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             onGround = false;
+            animPlayer.SetTrigger("Jump_trig");
         }
     }
 
@@ -36,10 +42,13 @@ public class PlayerController : MonoBehaviour
         {
             onGround = true;
         }
+        // Game is over if this condition is met
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            animPlayer.SetBool("Death_b", true);
+            animPlayer.SetInteger("DeathType_int", 1);
         }
     }
 }

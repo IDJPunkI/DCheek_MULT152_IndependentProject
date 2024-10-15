6571754e466f2ebbc9e460b6b9917d187bcaa7bb
IDJPunkI <138734945+IDJPunkI@users.noreply.github.com>
2024-10-15 12:02:42 -0700

    Golem Upgrades and Particles

--- DCheek_IndependentProject/Assets/Scripts/Golem.cs
+++ DCheek_IndependentProject/Assets/Scripts/Golem.cs
@@ -1,3 +1,4 @@
+using DigitalRuby.PyroParticles;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,42 @@ using UnityEngine;
 public class Golem : MonoBehaviour
 {
     private AudioSource[] audioSources;
+    private ParticleSystem particles;
+    private FireLightScript fireLight;
+    private FireConstantBaseScript fireConstant;
+
+    public bool upgrade = false;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSources = GetComponents<AudioSource>();
+        particles = GetComponentInChildren<ParticleSystem>();
+        particles.Stop();
         InvokeRepeating("Growl", 2.0f, 45.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (upgrade == true)
+        {
+            if (CompareTag("FireGolem"))
+            {
+                fireLight = GetComponentInChildren<FireLightScript>();
+                fireConstant = GetComponentInChildren<FireConstantBaseScript>();
+                fireLight.enabled = true;
+                fireConstant.enabled = true;
+            }
+
+            else
+            {
+                if (particles.isPlaying == false)
+                {
+                    particles.Play();
+                }
+            }
+        }
     }
 
     void Growl()

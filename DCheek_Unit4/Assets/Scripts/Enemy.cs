dce4f538aea31d3eabb04b1a7a0e5fbc424740c8
IDJPunkI <138734945+IDJPunkI@users.noreply.github.com>
2024-10-24 10:17:52 -0700

    Lesson B Finished

---
+++ DCheek_Unit4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody enemyRB;
+    GameObject player;
+    public float speed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        enemyRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 seekDirection = (player.transform.position - transform.position).normalized;
+        enemyRB.AddForce((player.transform.position - transform.position).normalized * speed * Time.deltaTime);
+    }
+}
